6f68811782f716f1c923670ba1cacca1d58f8ed6
Rishabh Teotia <34571994+deathrattle@users.noreply.github.com>
2019-08-23 14:33:42 +0530

    Update start.py

--- actions/start.py
+++ actions/start.py
@@ -1,14 +1,11 @@
 import requests
 from st2common.runners.base_action import Action
 
-class KennyAction (Action) :
-    def run(self, url) :
-        response = requests.get ( url )
+class KennyAction (Action):
+    def run(self, url):
+        response = requests.get( url )
         print ( response.status_code )
-
-    print ( response.url )
-
-
+        print (response.url)
     if (response==200):
         print ( 'Successful!' )
     else :
