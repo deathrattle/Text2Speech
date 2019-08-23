9dfcf3f4c4053ec0d1b8d3a3b78aad5177556992
Rishabh Teotia <34571994+deathrattle@users.noreply.github.com>
2019-08-23 15:26:31 +0530

    Update start.py

--- actions/start.py
+++ actions/start.py
@@ -1,13 +1,14 @@
+import sys
 import requests
 from st2common.runners.base_action import Action
 
 class KennyAction (Action):
     def run(self, url):
-        response = requests.get( url )
-        print ( response.status_code )
-        print (response.url)
-        if (response==200):
-            print ( 'Successful!' )
-        else :
-            print ( 'An error occurred.' )
+        try:
+                response = requests.get( url )
+                print ( response.status_code )
+                print (response.url)
+        except  requests.exceptions.MissingSchema:
+                print ( 'UnSuccessful!' )
+                sys.exit(0)
 
