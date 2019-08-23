69d786fd2bc50b72b9892a72e0b79b8b43f841da
Rishabh Teotia <34571994+deathrattle@users.noreply.github.com>
2019-08-23 14:06:53 +0530

    Update start.py

--- actions/start.py
+++ actions/start.py
@@ -1,10 +1,16 @@
 import requests
-class KennyAction(Action):
-def run(self, url):
-response= requests.get(url)
-	print(response.status_code)
-	print(response.url)
-if response:
-    print('Successful!')
-else:
-    print('An error occurred.')
+
+
+class KennyAction ( Action ) :
+    def run(self, url) :
+        response = requests.get ( url )
+        print ( response.status_code )
+
+    print ( response.url )
+
+
+if response :
+    print ( 'Successful!' )
+else :
+   print ( 'An error occurred.' )
+
